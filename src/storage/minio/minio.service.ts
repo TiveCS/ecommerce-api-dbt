87e0e2cb64d58c458bc3a68ea976270e2cff447c
TiveCS <ahmadfathanhanif@gmail.com>
2023-05-10 00:35:33 +0700

    implement minio connection

--- src/storage/minio/minio.service.ts
+++ src/storage/minio/minio.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as Minio from 'minio';
 
 @Injectable()
-export class MinioService {}
+export class MinioService extends Minio.Client {
+  constructor(private config: ConfigService) {
+    super({
+      accessKey: config.get('S3_ACCESS_KEY'),
+      secretKey: config.get('S3_SECRET_KEY'),
+      endPoint: config.get('S3_ENDPOINT'),
+    });
+  }
+}
