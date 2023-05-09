b9fb67721ca9936679bf69f0e2c8dd6c9d796fa1
TiveCS <ahmadfathanhanif@gmail.com>
2023-05-10 00:29:34 +0700

    initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './database/prisma.module';
 import { AuthModule } from './app/auth/auth.module';
+import { StorageModule } from './storage/storage.module';
+import { ProductModule } from './app/products/product.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    PrismaModule,
+    StorageModule,
+    AuthModule,
+    ProductModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
 })
 export class AppModule {}
