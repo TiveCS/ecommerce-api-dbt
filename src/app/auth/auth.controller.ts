3588293973d60aafdacf7f60ee7c0eb7fdfb66f6
TiveCS <ahmadfathanhanif@gmail.com>
2023-05-20 20:01:49 +0700

    Add response msg with same msg

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -44,7 +44,7 @@ export class AuthController {
   @UseGuards(JwtGuard)
   async check(@User() user: JwtUserType) {
     return {
-      message: 'Token is valid',
+      message: 'Token is valid and can access resource',
       status: 'success',
       data: {
         userId: user.identityId,
@@ -56,7 +56,7 @@ export class AuthController {
   @UseGuards(JwtGuard, MerchantOnlyGuard)
   async checkMerchant(@User() user: JwtUserType) {
     return {
-      message: 'Token is valid',
+      message: 'Token is valid Token is valid and can access resource',
       status: 'success',
       data: {
         userId: user.identityId,
